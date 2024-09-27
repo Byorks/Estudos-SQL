9561c80207513efc9370f848fd5c818172da3f57
VANESSA BYORK FERREIRA PINTO <52865485803@info.local>
2024-09-27 17:07:39 -0300

    :sparkles: feat: Banco de Dados Marvel Inserçoes

--- UniversoMarvelProfessor.sql
+++ UniversoMarvelProfessor.sql
@@ -1,7 +1,10 @@
+https://dontpad.com/senai234banco
+--------------------------------------------------
 
-CREATE DATABASE UniversoMarvel2;
 
-USE UniversoMarvel2;
+CREATE DATABASE UniversoMarvel;
+
+USE UniversoMarvel;
 
 -- Criando tabela Herois
 CREATE TABLE Herois (
@@ -109,68 +112,45 @@ INSERT INTO Herois_Viloes (ID_Heroi, ID_Vilao) VALUES
 (2, 2),  -- Capitão América x Caveira Vermelha 
 (3, 4),  -- Homem de Ferro x Loki
 (4, 1);  -- Thor x Thanos 
-	
-	
-INSERT INTO Batalhas (Local, ANO) VALUES
-('Nova York', 2012), --Batalha de Nova York
-('Sokovia', 2015); --Batalha de Sokovia
 
-SELECT * FROM Batalhas
+SELECT * FROM Herois_Batalhas
 
--- Quem lutou em Cada Batalha?
+-- Adicionando Batalhas Épicas
+INSERT INTO Batalhas (Local, Ano) VALUES
+('Nova York', 2012), -- Batalha de Nova York 
+('Sokovia', 2015); -- Batalha de Sokovia 
+
+
+-- Quem Lutou em Cada Batalha?
 INSERT INTO Herois_Batalhas (ID_Heroi, ID_Batalha) VALUES
-(1,1),(2,1),(3,1),(4,1),(6,1), -- Herois na Batalha de Nova York
-(1,2),(2,2),(3,2),(4,2),(5,2),(6,2),(7,2); -- Herois na Batalha de Sokovia
+(1, 1), (2, 1), (3, 1), (4, 1), (6, 1), -- Heróis na Batalha de Nova York
+(1, 2), (2, 2), (3, 2), (4, 2), (5, 2), (6, 2), (7, 2); -- Heróis na Batalha de Sokovia
 
 -- Os Vilões por trás das Batalhas
 INSERT INTO Viloes_Batalhas (ID_Vilao, ID_Batalha) VALUES
-(4,1), -- Loki na Batalha de Nova York
-(1,2); -- Thanos na Batalha de Sokovia
+(4, 1), -- Loki na Batalha de Nova York
+(1, 2); -- Thanos na Batalha de Sokovia
 
-SELECT
-	Viloes.Nome_Vilao,
-	Viloes.Habilidades
-FROM
-	Viloes
-JOIN Herois_Viloes ON Viloes.ID_Vilao = Herois_Viloes.ID_Vilao -- Pesquisando na tabela Herois e Viloes onde os IDs de Vilões sejam iguais aos Vilões da Tabela Herois e Viloes
-WHERE
-	Herois_Viloes.ID_Heroi = (SELECT ID_Heroi FROM Herois WHERE Herois.Nome_Heroi = 'Homem de Ferro');
+SELECT * FROM Viloes ORDER BY Nome_Vilao;
 
-DELETE FROM Herois WHERE ID_Heroi = 3;
+SELECT * FROM Viloes WHERE Ano_Criacao < 1980;
 
--- Criar e apagar colunas
-ALTER TABLE Herois DROP COLUMN Nome_Real; -- Apaga columa
-ALTER TABLE Herois ADD Nascionalidade VARCHAR (200); --Adiciona uma coluna na tabela Herois
-UPDATE Herois SET Planeta_Origem = 'Planeta X', Habilidades = 'Super Força Gama' WHERE Nome_Herois = 'Hulk';
+SELECT DISTINCT Planeta_Origem FROM Herois; -- 
 
--- É possível transformar um registro em Primary Key, ele tem que ser NOT NUll
+SELECT Nome_Heroi, Habilidades FROM Herois WHERE Habilidades LIKE '%Super força%'; --com determinado conteúdo
 
-sp_rename 'Herois.Planeta_Origem', 'PlanetaNatal';
+SELECT Nome_Heroi, Planeta_Origem FROM Herois WHERE Planeta_Origem <> 'Terra'; -- diferença
 
-sp_rename 'Viloes', 'Antagonistas';
+SELECT * FROM Viloes ORDER BY Ano_Criacao ASC; --Ordenar uma tabela por ordem ascendente 
+SELECT * FROM Viloes ORDER BY Ano_Criacao DESC; -- Ordenar uma tabela por ordem descendente 
 
-SELECT v.Nome_Vilao
-FROM Viloes v 
-JOIN Herois_Viloes hv ON v.Nome_Vilao = hv.ID_Vilao
-JOIN Herois h ON hv.ID_Heroi = h.ID_Heroi
+-- Salvar esse arquivo localmente no seu pc, na pasta com seu nome, use o nome de "DB_UniversoMarvel_V2.sql" 
+-- Anexar e entregar no classroom no dia de ontem e de hoje, dias 25 e 26 de setembro/24
+-- fim da aula 26/09
 
-INSERT INTO Herois (Nome_Heroi, Nome_Real, Ano_Criacao, Planeta_Origem, Habilidades) VALUES
-('Wolverine', 'Logan', 1974, 'Terra', 'Fator cura acelerado, Garras retráteis de adamantium, sentidos aguçados');
 
-SELECT * FROM Herois
 
-INSERT INTO Equipes (Nome_Equipe, Base_Operacoes) VALUES
-('Quarteto Fantástico', 'Edifício Baxter');
 
-SELECT * FROM Equipes
 
-INSERT INTO Viloes (Nome_Vilao, Ano_Criacao, Habilidades) VALUES 
-('Magneto', 1963, 'Controle do magnetismo');
 
--- Descobrindo o ID do Wolverine e dos X-Men
-
-INSERT INTO Herois_Equipes (ID_Heroi, ID_Equipe) VALUES
-(8,2);
-
-DECLARE @WolverineID INT; 
-SELECT @WolverineID = ID_Heroi FROM Heroix WHERE Nome_Heroi = 'Wolverine'
+								
